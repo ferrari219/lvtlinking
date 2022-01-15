5c121391ed268cfa71eea2775cdb5fdb3ab56416
ferrari219@nate.com <ferrari219@nate.com>
2022-01-15 23:46:24 +0900

    footer 제작

--- src/components/02_Molecules/TabUl/TabUl.jsx
+++ src/components/02_Molecules/TabUl/TabUl.jsx
@@ -5,54 +5,54 @@ import theme from 'styles/theme';
 import { Link } from 'react-router-dom';
 
 const TabUl = () => {
-  return (
-    <ul css={tabulStyle}>
-      {data &&
-        data.map(({ id, name, lnk }) => (
-          <li key={id}>
-            <Link to={lnk}>{name}</Link>
-          </li>
-        ))}
-    </ul>
-  );
+	return (
+		<ul css={tabulStyle}>
+			{data &&
+				data.map(({ id, name, lnk }) => (
+					<li key={id}>
+						<Link to={lnk}>{name}</Link>
+					</li>
+				))}
+		</ul>
+	);
 };
 
 const data = [
-  {
-    id: 0,
-    name: '클래스',
-    lnk: '/',
-  },
-  {
-    id: 1,
-    name: '플리마켓',
-    lnk: '/',
-  },
+	{
+		id: 0,
+		name: '클래스',
+		lnk: '/',
+	},
+	{
+		id: 1,
+		name: '플리마켓',
+		lnk: '/',
+	},
 ];
 
 const tabulStyle = css`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  width: 100%;
-  & > li {
-    flex: 1;
-    /* border: 1px solid red; */
-    a {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-      font-size: ${theme.size.lg};
-      font-weight: 400;
-    }
-    &:hover,
-    &.is-active {
-      a {
-        color: ${theme.color.main};
-      }
-    }
-  }
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	width: 100%;
+	& > li {
+		flex: 1;
+		/* border: 1px solid red; */
+		a {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 100%;
+			font-size: ${theme.size.lg};
+			font-weight: 400;
+		}
+		&:hover,
+		&.is-active {
+			a {
+				color: ${theme.color.main};
+			}
+		}
+	}
 `;
 
 export default TabUl;
--- src/components/03_Organisms/Footer/Footer.jsx
+++ src/components/03_Organisms/Footer/Footer.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import Logo from 'assets/img/logo.png';
+import theme from 'styles/theme';
 
 const Footer = () => {
-  return <div css={footerStyle}>footer</div>;
+	return (
+		<div css={footerStyle}>
+			<div className="left">
+				<img src={Logo} alt="LivartLinking" />
+			</div>
+			<div className="right">
+				Copyright ⓒ2022 LivartLinking inc, ltd. All rights reserved
+			</div>
+		</div>
+	);
 };
 
-const footerStyle = css``;
+const footerStyle = css`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	margin-top: 2rem;
+	padding: 2rem;
+	border-top: 1px solid #ddd;
+	& > .left {
+		flex: 0 0 30rem;
+		img {
+			width: 15rem;
+			filter: grayscale(100%);
+			opacity: 0.8;
+		}
+	}
+	& > .right {
+		flex: 1;
+		font-size: ${theme.size.sm};
+		color: #666;
+	}
+`;
 
 export default Footer;
